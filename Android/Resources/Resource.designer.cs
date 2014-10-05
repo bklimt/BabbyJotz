963b20ba6bc7bc48f6e8fb204ce0e1a90c8f523c
Bryan Klimt <bklimt@gmail.com>
2014-10-04 22:05:49 -0700

    Make the tabs dark.

--- Android/Resources/Resource.designer.cs
+++ Android/Resources/Resource.designer.cs
@@ -63,22 +63,6 @@ namespace BabbyJotz.Android
 			}
 		}
 		
-		public partial class Id
-		{
-			
-			// aapt resource value: 0x7f060000
-			public const int myButton = 2131099648;
-			
-			static Id()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Id()
-			{
-			}
-		}
-		
 		public partial class Layout
 		{
 			
@@ -98,11 +82,8 @@ namespace BabbyJotz.Android
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
-			
 			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
@@ -117,11 +98,14 @@ namespace BabbyJotz.Android
 		public partial class Style
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int BabbyActionBar = 2131034113;
+			
 			// aapt resource value: 0x7f050000
 			public const int BabbyTextView = 2131034112;
 			
-			// aapt resource value: 0x7f050001
-			public const int BabbyTheme = 2131034113;
+			// aapt resource value: 0x7f050002
+			public const int BabbyTheme = 2131034114;
 			
 			static Style()
 			{
