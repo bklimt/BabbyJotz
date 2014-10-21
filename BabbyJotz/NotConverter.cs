784482b2e5aedbf28245ade91a5a789049ddae35
Bryan Klimt <bklimt@gmail.com>
2014-10-20 23:17:01 -0700

    Make the number binding work a little smoother.

--- BabbyJotz/NotConverter.cs
+++ BabbyJotz/NotConverter.cs
@@ -3,20 +3,20 @@ using System.Globalization;
 using Xamarin.Forms;
 
 namespace BabbyJotz {
-	public class NotConverter : IValueConverter {
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-			if (value is Boolean) {
-				return !(bool)value;
-			}
-			return value;
-		}
+    public class NotConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value is Boolean) {
+                return !(bool)value;
+            }
+            return value;
+        }
 
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
-			if (value is Boolean) {
-				return !(bool)value;
-			}
-			return value;
-		}
-	}
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value is Boolean) {
+                return !(bool)value;
+            }
+            return value;
+        }
+    }
 }
 
---
+++ BabbyJotz/NumberConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace BabbyJotz {
+    public class NumberConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value is decimal && targetType == typeof(string)) {
+                if ((decimal)value == 0.0m) {
+                    return "";
+                }
+                return String.Format("{0}", value);
+            }
+            if (value is double && targetType == typeof(string)) {
+                if ((double)value == 0.0) {
+                    return "";
+                }
+                return String.Format("{0}", value);
+            }
+            throw new InvalidOperationException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value is string && targetType == typeof(decimal)) {
+                var result = 0.0m;
+                Decimal.TryParse((string)value, out result);
+                return result;
+            }
+            if (value is string && targetType == typeof(double)) {
+                var result = 0.0;
+                Double.TryParse((string)value, out result);
+                return result;
+            }
+            throw new InvalidOperationException();
+        }
+    }
+}
+
